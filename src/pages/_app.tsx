b6d425562d31aa546111f78e9f2091d44ec38fa4
PengMouse <tundejoseph294@gmail.com>
2025-12-24 14:06:32 +0100

    feat: fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			<Head>
 				<title>Benagos, The Product Company</title>
 			</Head>
-			<Component {...pageProps} />;
+			<Component {...pageProps} />
 		</>
 	);
 }
